6fc1fb3985c9cd373bc2ba9a0d53e1e01b0011ff
32 кабинет <ss_slavich@mail.ru>
2023-01-19 18:29:33 +0300

    Refacted useEffect

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -10,7 +10,7 @@ const Cards: FC = () => {
 
   useEffect(() => {
     productsAPI.getProducts().then((result: any) => setCards(result));
-  }, [cards]);
+  }, []);
 
   return (
     <div className={s.container}>
